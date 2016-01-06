bd3630843a6ff0ccdfbe0439cac39bb21108bab7
Ryan <ryanwatson501@gmail.com>
2016-01-05 19:41:03 -0600

    Better logging

--- PublicIPLogger/Helper.cs
+++ PublicIPLogger/Helper.cs
@@ -10,16 +10,19 @@ namespace PublicIPLogger
 {
     class Helper
     {
+        private string lastIP = "";
+        private string currentIP = "";
         public void getPublicIP()
         {
             WebRequest req;
             WebResponse resp;
+            int timeBetweenRequestsMS = 100000;
+            string url = "http://checkip.dyndns.org";
             EventLog myLogger = new EventLog("PublicIPLogger", ".", "PublicIPLogger");
             int i = 0;
             while (0 != 1)
             {
-                Thread.Sleep(10000);
-                string url = "http://checkip.dyndns.org";
+                Thread.Sleep(timeBetweenRequestsMS);
                 try
                 {
                     req = WebRequest.Create(url);
@@ -29,9 +32,20 @@ namespace PublicIPLogger
                     string[] a = response.Split(':');
                     string a2 = a[1].Substring(1);
                     string[] a3 = a2.Split('<');
-                    string a4 = a3[0];
-
-                    myLogger.WriteEntry(Convert.ToString(a4), EventLogEntryType.Information, 1010, 1);
+                    //string a4 = a3[0];
+                    currentIP = Convert.ToString(a3[0]);
+                    if (i > 0)
+                    {
+                        if (currentIP != lastIP)
+                        {
+                            myLogger.WriteEntry(currentIP, EventLogEntryType.Information, 1012, 1);
+                        }
+                    }
+                    else
+                    {
+                        myLogger.WriteEntry(currentIP, EventLogEntryType.Information, 1010, 1);
+                    }
+                    lastIP = currentIP;
                     i++;
 
                 }
